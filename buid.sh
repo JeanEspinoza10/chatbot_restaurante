eb4f5af600e4ac3305d5c1eb83387470491c8223
JeanEspinoza10 <jeanria10@gmail.com>
2023-03-11 17:24:57 -0500

    subiendo

--- buid.sh
+++ buid.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
-
+pip install --upgrade pip
 pip install -r requirements.txt
-pip install -r requirements.txt
-flask db init
-flask db migrate -m "Comentario"
 flask db upgrade
 flask seed run
+
