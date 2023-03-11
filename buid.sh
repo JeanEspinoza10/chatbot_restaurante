e226c89cfb3c7d8a465aa6c89e7a72cbffe15586
JeanEspinoza10 <jeanria10@gmail.com>
2023-03-11 17:20:32 -0500

    subiendo

--- buid.sh
+++ buid.sh
@@ -4,5 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 pip install -r requirements.txt
+flask db init
+flask db migrate -m "Comentario"
 flask db upgrade
 flask seed run
--- config.py
+++ config.py
@@ -1,17 +1,17 @@
-from os import getenv
+import os
 from datetime import timedelta
 
 
 class BaseConfig:
-    SQLALCHEMY_DATABASE_URI = getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = getenv('JWT_SECRET')
+    JWT_SECRET_KEY = os.getenv('JWT_SECRET')
 
-    MAIL_SERVER = getenv('MAIL_SERVER')
-    MAIL_PORT = getenv('MAIL_PORT')
-    MAIL_USE_TLS = getenv('MAIL_USE_TLS')
-    MAIL_USERNAME = getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = getenv('MAIL_PASSWORD')
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = os.getenv('MAIL_PORT')
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS')
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
 
 
 class DevelopmentConfig(BaseConfig):
